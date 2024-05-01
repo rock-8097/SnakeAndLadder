2506a96b2d0039670462cf540e71e933eddaea68
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-05-01 18:02:40 +0530

    Repeat till player reach position 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,30 +5,32 @@ public class SnakeAndLadder {
     public static void main(String[] args){
         System.out.println("Welcome to Snake and ladder Simulation");
         int player1 = 0;// First Player was created
-        int poss = positionChangeOrNot(player1);// call function position
-        // Generate a random number between 1 and 6
-        int dice = rand.nextInt(6) + 1;
-        if (poss==1){
-            // If we get Snake then Subtract with dice number
-            player1 = player1 - dice;
-        }else if(poss==2){
-            // If we get Snake then Add with dice number
-            player1 = player1 + dice;
+        while(player1<=100){//loop to player reach 100 position
+            int poss = positionChangeOrNot(player1);// call function position
+            // Generate a random number between 1 and 6
+            int dice = rand.nextInt(6) + 1;
+            if (poss==1){
+                // If we get Snake then Subtract with dice number
+                player1 = player1 - dice;
+            }else if(poss==2){
+                // If we get Snake then Add with dice number
+                player1 = player1 + dice;
+            }
+            System.out.print(player1+"\t");
         }
-        System.out.println(player1);
     }
     public static int positionChangeOrNot(int x){
         // Generating Randome number for NO Play, Snake and Lader
         int possition = rand.nextInt(3);
         switch (possition) {
             case 0:
-                System.out.println("No Play");
+                System.out.print("No Play\t");
                 break;
             case 1:
-                System.out.println("Snake");
+                System.out.print("Snake\t");
                 break;
             case 2:
-                System.out.println("Ladder");
+                System.out.print("Ladder\t");
                 break;
             default:
                 break;
