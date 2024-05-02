fcce4c656e835421de4708f6b1f6952a5d8f307a
Harshad Tembhe <Harshadtembhe1234@gmail.com>
2024-05-02 11:39:28 +0530

    Player roll dice to get number 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,13 @@
+import java.util.Random;
+
 public class SnakeAndLadder {
+    public static Random rand = new Random();
     public static void main(String[] args){
-        System.out.print("Welcome to Snake and ladder Simulation");
+        System.out.println("Welcome to Snake and ladder Simulation");
         int player1 = 0;// First Player was created
+        // Generate a random number between 1 and 6
+        int dice = rand.nextInt(6) + 1;
+        System.out.println(player1+dice);
     }
+   
 }
